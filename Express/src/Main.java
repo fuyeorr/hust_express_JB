cc133738c3dd1409953991d742c805b481c0a3d4
shappy-66 <1125979074@qq.com>
2026-01-03 22:55:41 +0800

    add shappy

--- Express/src/Main.java
+++ Express/src/Main.java
@@ -2,7 +2,7 @@ import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
-        System.out.printf("Hello and welcome!\n");
+        System.out.printf("Hello and welcome，shappy!\n");
 
         DBUtil.getConnection();
     }
