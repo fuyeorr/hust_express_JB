a9f5f7daf0e83031152939d471f5e6cc5c67d8af
fuyeorr <itclkng@163.com>
2026-01-06 20:12:08 +0800

    add file ui & add ui in Main.java

--- Express/src/Main.java
+++ Express/src/Main.java
@@ -1,11 +1,22 @@
 import java.sql.Connection;
 import java.sql.SQLException;
 
+import javax.swing.SwingUtilities;
+import src.ui.MainWindow;
+
 public class Main {
     public static void main(String[] args) throws SQLException {
         System.out.printf("Hello and welcome，shappy!\n");
 
         Connection con = DBUtil.getConnection();
         DBUtil.close(con, null, null);
+
+        // Launch GUI
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new MainWindow();
+            }
+        });
     }
 }
